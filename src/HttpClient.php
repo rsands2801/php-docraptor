004f30f1fbd3ad27c2cc88dee20ee1046679956a
rsands2801 <richard@it-partner.co.uk>
2015-08-21 16:05:25 +0100

    add doGet()

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -75,6 +75,48 @@ class HttpClient implements HttpTransferInterface
     }
 
     /**
+     * @param string $uri
+     * @return mixed
+     * @throws Exception
+     */
+    public function doGet($uri)
+    {
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $uri);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        if ($this->config->getReportUserAgent()) {
+            curl_setopt($ch, CURLOPT_USERAGENT, $this->userAgent());
+        }
+        $result = curl_exec($ch);
+
+        if (!$result) {
+            throw new Exception(sprintf('Curl error: %s', curl_error($ch)));
+        }
+
+        $httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if ($httpStatusCode != 200) {
+            switch ($httpStatusCode) {
+                case 400:
+                    throw new BadRequestException();
+                case 401:
+                    throw new UnauthorizedException();
+                case 403:
+                    throw new ForbiddenException();
+                case 422:
+                    throw new UnprocessableEntityException();
+                default:
+                    throw new UnexpectedValueException($httpStatusCode);
+            }
+        }
+
+        curl_close($ch);
+
+        return $result;
+    }
+
+    /**
      * If you extend this software via a fork or private copy but use it to talk to production
      * DocRaptor servers, please change the userAgent string to reflect your new repo's name
      * or at least differ from the official wrapper.
